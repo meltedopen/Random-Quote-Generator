61790b9a02ad7e97be65bdeac9d42da39a0a05c9
Isaac <isaac.chang5@gmail.com>
2019-05-23 16:43:36 -0700

    working random code generator

--- js/script.js
+++ js/script.js
@@ -5,52 +5,72 @@ project 1 - A Random Quote Generator
 
 // Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
 
+const quotes = [
+  {
+    quote: 'The only true wisdom is in knowing you know nothing.',
+    source: 'Socrates',
+    citation: 'Apology 29b-c',
+    year: '410 BC'
+  },
+  {
+    quote: 'Problems are not stop signs, they are guidelines.',
+    source: 'Robert H. Schuller'
+  },
+  {
+    quote: 'No act of kindness, no matter how small, is ever wasted.',
+    source: 'Aesop',
+    year: '600 BC'
+  },
+  {
+    quote: 'There is nothing permanent except change.',
+    source: 'Heraclitus'
+  },
+  {
+    quote: 'All that we see or seem is but a dream within a dream.',
+    source: 'Edgar Allan Poe',
+    citation: 'The Flag Of Our Union',
+    year: '1849'
+  }
+];
 
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
+console.log(quotes);
 
+// Random BG color
+const random_bg_color = () => {
+  let x = Math.floor(Math.random() * 256);
+  let y = Math.floor(Math.random() * 256);
+  let z = Math.floor(Math.random() * 256);
+  let bgColor = 'rgb(' + x + ',' + y + ',' + z + ')';
+  console.log(bgColor);
 
+  document.body.style.background = bgColor;
+};
 
+//Get Random Quote
+const getRandomQuote = () => {
+  let randomNum = Math.floor(Math.random() * quotes.length);
+  return quotes[randomNum];
+};
 
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
+//Print Random Quote
+const printQuote = () => {
+  let randomQuote = getRandomQuote();
+  let htmlString = '';
+  htmlString += `<p class ='quote'>${randomQuote.quote}</p>`;
+  htmlString += `<p class ='source'>${randomQuote.source}`;
+  if (randomQuote.citation) {
+    htmlString += `<span class ='citation'>${randomQuote.citation}</span>`;
+  }
+  if (randomQuote.year) {
+    htmlString += `<span class ='year'>${randomQuote.year}</span>`;
+  }
+  htmlString += '</p>';
+  document.getElementById('quote-box').innerHTML = htmlString;
+  random_bg_color();
+};
 
-
-
-
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
-
-
-
-
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
-
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
+document
+  .getElementById('loadQuote')
+  .addEventListener('click', printQuote, false);
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
